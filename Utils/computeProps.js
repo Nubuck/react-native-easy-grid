a5da2c95e425c784bdf414c07cb3a1a5409187a5
Barry Buck <jozi011@gmail.com>
2016-06-01 09:26:08 +0200

    fixed missing lodashg require && version bump

--- Utils/computeProps.js
+++ Utils/computeProps.js
@@ -1,40 +1,43 @@
 var React = require('react');
 
-var ReactNativePropRegistry = require('react-native/Libraries/ReactNative/ReactNativePropRegistry');
+var ReactNativePropRegistry = require(
+  'react-native/Libraries/ReactNative/ReactNativePropRegistry');
+
+var _ = require('lodash');
 
 module.exports = function(incomingProps, defaultProps) {
 
 
-    // External props has a higher precedence
-    var computedProps = {};
+  // External props has a higher precedence
+  var computedProps = {};
 
-    incomingProps = _.clone(incomingProps);
-    delete incomingProps.children;
+  incomingProps = _.clone(incomingProps);
+  delete incomingProps.children;
 
-    // console.log(defaultProps, incomingProps);
+  // console.log(defaultProps, incomingProps);
 
-    if(incomingProps)
-        _.merge(computedProps, defaultProps, incomingProps);
-    else
-        computedProps = defaultProps;
+  if (incomingProps)
+    _.merge(computedProps, defaultProps, incomingProps);
+  else
+    computedProps = defaultProps;
 
-    // Pass the merged Style Object instead
-    if(incomingProps.style) {
+  // Pass the merged Style Object instead
+  if (incomingProps.style) {
 
-        if(typeof incomingProps.style == 'number') {
-            var incomingPropsStyle = ReactNativePropRegistry.getByID(incomingProps.style);
-            computedProps.style = {};
-        } else {
-            var incomingPropsStyle = incomingProps.style;
-        }
-
-        _.merge(computedProps.style, defaultProps.style, incomingPropsStyle);
-
+    if (typeof incomingProps.style == 'number') {
+      var incomingPropsStyle = ReactNativePropRegistry.getByID(incomingProps.style);
+      computedProps.style = {};
+    } else {
+      var incomingPropsStyle = incomingProps.style;
     }
 
-    // console.log("computedProps ", computedProps);
+    _.merge(computedProps.style, defaultProps.style, incomingPropsStyle);
 
-    return computedProps;
+  }
+
+  // console.log("computedProps ", computedProps);
+
+  return computedProps;
 
 
 }
